3b5d875f91b87095f2a2cb4290069b5103c557bd
Udinei Silva <ukaliko@gmail.com>
2019-11-28 17:26:31 -0400

    Iniciando a Aplicacao front-end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import Login from './views/login';
+
+import 'bootswatch/dist/flatly/bootstrap.css'
+import './custom.css'
+
+/** Criando componente em forma de classe */
+class App extends React.Component{
+   render(){
+    return (
+      <div>
+        <Login />
+      </div>
+    );  
+  }
+}
+
+/**Criando  componente Reactjs em forma de funcao
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello world!
+   
     </div>
   );
 }
-
+ */
 export default App;
